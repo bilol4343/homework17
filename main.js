242e1e495ede7a04d5a142c78d56abc8a884613d
bilol4343 <wwpubg113@gmail.com>
2025-09-07 04:03:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
-const counterValue = document.getElementById("counter-value");
-const decreaseBtn = document.getElementById("decrease");
-const increaseBtn = document.getElementById("increase");
-const resetBtn = document.getElementById("reset");
+const num1El = document.getElementById("num1")
+const num2El = document.getElementById("num2")
+const operatorEl = document.getElementById("operator")
+const resultBox = document.getElementById("resultBox")
+const calculateBtn = document.getElementById("calculate")
+const resetBtn = document.getElementById("reset")
 
-let count = 0;
+calculateBtn.addEventListener("click", function() {
+  const num1 = parseFloat(num1El.value);
+  const num2 = parseFloat(num2El.value);
+  const operator = operatorEl.value;
+  let result = "";
 
-increaseBtn.addEventListener("click", () => {
-  count++;
-  counterValue.innerText = count;
+  if (isNaN(num1) || isNaN(num2)) {
+    result = " Ikkala sonni ham kiriting!";
+  } else {
+    switch (operator) {
+      case "add":
+        result = num1 + num2;
+        break;
+      case "minus":
+        result = num1 - num2;
+        break;
+      case "multiply":
+        result = num1 * num2;
+        break;
+      case "divide":
+        result = num2 !== 0 ? (num1 / num2) : " Nolga bolib bolmaydi!";
+        break;
+      default:
+        result = " Operator tanlanmadi!";
+    }
+  }
+  resultBox.innerText = "Result: " + result;
 });
 
-decreaseBtn.addEventListener("click", () => {
-  count--;
-  counterValue.innerText = count;
-});
-
-resetBtn.addEventListener("click", () => {
-  count = 0;
-  counterValue.innerText = count;
+resetBtn.addEventListener("click", function() {
+  num1El.value = "";
+  num2El.value = "";
+  operatorEl.value = "";
+  resultBox.innerText = "Result:";
 });
